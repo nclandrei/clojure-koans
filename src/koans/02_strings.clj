3c93cf130f2d7ac8e67dea8fe0ccc352d5840be2
Andrei-Mihai Nicolae <nclandreimihai@gmail.com>
2019-03-16 23:04:22 +0100

    Implement part of the 2nd koan

--- src/koans/02_strings.clj
+++ src/koans/02_strings.clj
@@ -4,31 +4,31 @@
 
 (meditations
   "A string is nothing more than text surrounded by double quotes"
-  (= __ "hello")
+  (= "hello" "hello")
 
   "But double quotes are just magic on top of something deeper"
-  (= __ (str 'world))
+  (= "world" (str 'world))
 
   "You can do more than create strings, you can put them together"
-  (= "Cool right?" (str __ __))
+  (= "Cool right?" (str "Cool " "right?"))
 
   "You can even get certain characters"
-  (= \C (get "Characters" __))
+  (= \C (get "Characters" 0))
 
   "Or even count the characters"
-  (= __ (count "Hello World"))
+  (= 11 (count "Hello World"))
 
   "But strings and characters are not the same"
-  (= __ (= \c "c"))
+  (= false (= \c "c"))
 
   "What if you only wanted to get part of a string?"
   (= "World" (subs "Hello World" __ __))
 
   "How about joining together elements in a list?"
-  (= __ (string/join '(1 2 3)))
+  (= "1 2 3" (string/join '(1 2 3)))
 
   "What if you wanted to separate them out?"
-  (= "1, 2, 3" (string/join __ '(1 2 3)))
+  (= "1, 2, 3" (string/join "," '(1 2 3)))
 
   "Maybe you want to separate out all your lines"
   (= [__ __ __] (string/split-lines "1\n2\n3"))
@@ -46,16 +46,16 @@
   (= __ (string/index-of "hello world" "bob"))
 
   "Sometimes you don't want whitespace cluttering the front and back"
-  (= __ (string/trim "  \nhello world \t \n"))
+  (= "hello world" (string/trim "  \nhello world \t \n"))
 
   "You can check if something is a char"
-  (= __ (char? \c))
+  (= true (char? \c))
 
   "But it may not be"
-  (= __ (char? "a"))
+  (= false (char? "a"))
 
   "But chars aren't strings"
-  (= __ (string? \b))
+  (= false (string? \b))
 
   "Strings are strings"
   (= true (string? __))
